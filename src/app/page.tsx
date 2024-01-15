a3a1a36ca067b688f7524d359ad344d09f47df22
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-01-15 21:47:09 +0800

    Changes: Added FormPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { FormPage } from "./components/Form/page";
 import { TablePage } from "./components/Table/TablePage";
 
 export default function Home() {
   return (
     <main>
+      <section className="p-5">
+        <FormPage />
+      </section>
       <TablePage />
     </main>
   );
