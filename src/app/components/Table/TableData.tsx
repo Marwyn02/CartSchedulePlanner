806170d0ba6f94f5a75dc9caf340fed9a7905c6c
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-02-11 21:48:55 +0800

    Changes: Dynamic content on tableData

--- src/app/components/Table/TableData.tsx
+++ src/app/components/Table/TableData.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 
-export const TableData = () => {
+export const TableData = ({
+  schedules,
+  currentDate,
+}: {
+  schedules: any;
+  currentDate: string;
+}) => {
+  const filteredSchedule = schedules.filter(
+    (schedule: any) => schedule.date === currentDate
+  );
+
+  const data =
+    filteredSchedule.length > 0 ? (
+      filteredSchedule.map((s: any, i: any) => (
+        <div key={i}>
+          <p>
+            {i + 1}. {s.username}
+          </p>
+        </div>
+      ))
+    ) : (
+      <div></div>
+    );
+
   return (
     <td className="py-2 px-2 md:py-4 md:px-6 text-gray-800 font-medium">
-      <p>1. Marwyn</p>
-      <p>2. Jhun</p>
-      <p>3. Sumargo</p>
+      {data}
     </td>
   );
 };
