a1b28ac71396654030d8abe274e559a9cd2ce921
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-02-12 21:52:36 +0800

    Changes: Added Include true to also get the relation model

--- src/app/api/appointment/get.ts
+++ src/app/api/appointment/get.ts
@@ -1,11 +1,14 @@
 "use server";
 
-import { PrismaClient, Appointment } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default async function getAppointment() {
-  const appointments: Appointment[] = await prisma.appointment.findMany();
+  const appointments: any = await prisma.appointment.findMany({
+    include: { user: true },
+  });
+
   return {
     schedules: appointments,
   };
