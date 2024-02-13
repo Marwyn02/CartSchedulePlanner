bc12403f44b392c0bd2f7b29b83387cfacd27a5c
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-02-13 21:32:01 +0800

    Changes: Just changed it, no important matter here

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { FormPage } from "../components/Form/page";
 
 const SchedulePage = () => {
+  async function createAppointment() {
+    "use server";
+  }
   return <FormPage />;
 };
 
