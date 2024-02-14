9e81278de487f5cd701a53eb70aa73aa51da5228
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-02-14 22:44:15 +0800

    Changes: Filtering the names by each date, in tableData

--- src/app/components/Table/TableData.tsx
+++ src/app/components/Table/TableData.tsx
@@ -7,23 +7,15 @@ export const TableData = ({
   schedules: any;
   currentDate: string;
 }) => {
-  const filteredSchedule = schedules.filter(
-    (schedule: any) => schedule.date === currentDate
-  );
+  const filteredSchedule = schedules.filter((s: any) => s.date === currentDate);
 
-  const data =
-    filteredSchedule.length > 0 ? (
-      filteredSchedule.map((s: any, i: any) => (
-        <div key={i}>
-          <p>
-            {i + 1}. {s.username}
-          </p>
-        </div>
-      ))
-    ) : (
-      <div></div>
-    );
-
+  const data = filteredSchedule.map((s: any, i: any) => (
+    <div key={i}>
+      <p>
+        {i + 1}. {s.user[0].name}
+      </p>
+    </div>
+  ));
   return (
     <td className="py-2 px-2 md:py-4 md:px-6 text-gray-800 font-medium">
       {data}
