98f2ea9a1d78d899f23f9a0d482b24bf8e7fb2db
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-02-16 17:02:37 +0800

    Added: Place.ts

---
+++ src/app/api/place.ts
@@ -0,0 +1,26 @@
+export const place = () => {
+  const places = [
+    {
+      id: 1,
+      place: "Palengke A",
+    },
+    {
+      id: 2,
+      place: "Palengke B",
+    },
+    {
+      id: 3,
+      place: "LRT Circle",
+    },
+    {
+      id: 4,
+      place: "Sunshine",
+    },
+    {
+      id: 5,
+      place: "Alta Tierra",
+    },
+  ];
+
+  return places;
+};
