7053aef7ed2c68e2cb46c28594fc50d038dcd226
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-02-23 22:21:49 +0800

    Changes: Added selectedTime for morning and afternnon table

--- src/app/components/Table/TablePage.tsx
+++ src/app/components/Table/TablePage.tsx
@@ -4,25 +4,28 @@ import React from "react";
 import TableBody from "./TableBody";
 
 type TScheduleProps = {
-  schedules: {
-    id: number;
-    username: string;
-    date: string;
-  }[];
-};
+  id: number;
+  username: string;
+  time: string;
+  place: string;
+  date: string;
+}[];
 
 export const TablePage = ({
   appointments,
+  selectedTime,
 }: {
   appointments: TScheduleProps;
+  selectedTime: string;
 }) => {
-  const { schedules } = appointments;
-
   return (
     <section className="my-5 md:m-5">
+      <p className="mt-5 px-4 text-sm font-bold pb-2 text-gray-800 uppercase dark:text-gray-200">
+        {selectedTime}
+      </p>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-gray-100">
-          <TableBody schedules={schedules} />
+          <TableBody schedules={appointments} />
         </table>
       </div>
     </section>
