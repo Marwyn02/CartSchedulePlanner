8acb2a084c5123a2eafbde953646bd17c162ac93
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-02-23 22:23:38 +0800

    Changes Use Place API, looped the tableData and filtered

--- src/app/components/Table/TableBody.tsx
+++ src/app/components/Table/TableBody.tsx
@@ -2,51 +2,44 @@ import React from "react";
 
 import { TableData } from "./TableData";
 import { date } from "@/app/api/date";
+import { place } from "@/app/api/place";
 
-const cartPlaces = [
-  {
-    place: "Palengke A",
-  },
-  {
-    place: "Palengke B",
-  },
-  {
-    place: "LRT Circle",
-  },
-  {
-    place: "Sunshine",
-  },
-  {
-    place: "Alta Tierra",
-  },
-];
+type TScheduleProps = {
+  id: number;
+  username: string;
+  time: string;
+  place: string;
+  date: string;
+}[];
 
-const TableBody = ({ schedules }: { schedules: any }) => {
+const TableBody = ({ schedules }: { schedules: TScheduleProps }) => {
   const weekDates = date();
+  const cart = place();
 
-  const tableDatas = (
-    <>
-      <TableData schedules={schedules} currentDate={weekDates[0]} />
-      <TableData schedules={schedules} currentDate={weekDates[1]} />
-      <TableData schedules={schedules} currentDate={weekDates[2]} />
-      <TableData schedules={schedules} currentDate={weekDates[3]} />
-      <TableData schedules={schedules} currentDate={weekDates[4]} />
-      <TableData schedules={schedules} currentDate={weekDates[5]} />
-      <TableData schedules={schedules} currentDate={weekDates[6]} />
-    </>
+  const cartSchedule = cart.map(
+    ({ id, place }: { id: number; place: string }): React.ReactNode => {
+      const filteredSchedule = schedules.filter(
+        (schedule: { place: string }) => schedule.place === place
+      );
+
+      const tableData = weekDates.map((w: string, j: number) => (
+        <TableData key={j} schedules={filteredSchedule} currentDate={w} />
+      ));
+
+      return (
+        <tr className="border-b border-gray-200 dark:border-gray-800" key={id}>
+          <th
+            scope="row"
+            className="px-6 py-4 font-semibold text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+          >
+            {place}
+          </th>
+          {tableData}
+        </tr>
+      );
+    }
   );
 
-  const tableContent = cartPlaces.map((place: any, i: any) => (
-    <tr className="border-b border-gray-200 dark:border-gray-700" key={i}>
-      <th
-        scope="row"
-        className="px-6 py-4 font-semibold text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
-      >
-        {place.place}
-      </th>
-      {tableDatas}
-    </tr>
-  ));
   return (
     <>
       <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
@@ -83,7 +76,7 @@ const TableBody = ({ schedules }: { schedules: any }) => {
           </th>
         </tr>
       </thead>
-      <tbody>{tableContent}</tbody>
+      <tbody>{cartSchedule}</tbody>
     </>
   );
 };
