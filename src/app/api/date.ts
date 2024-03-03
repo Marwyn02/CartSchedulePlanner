6942ec959b0518e0d4b6ff63ee8cbe533c6f7120
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-03-03 21:54:15 +0800

    API: previous week days date

--- src/app/api/date.ts
+++ src/app/api/date.ts
@@ -33,3 +33,42 @@ export const date = () => {
 
   return datesOfWeek;
 };
+
+export const oldDate = () => {
+  const getStartOfPreviousWeek = () => {
+    const today = new Date();
+    const previousDayOfWeek = new Date(today);
+
+    const dayOfWeek = previousDayOfWeek.getDay();
+    const difference = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+
+    const previousDayOfWeekTimestamp =
+      previousDayOfWeek.getTime() - (difference + 7) * 24 * 60 * 60 * 1000;
+    previousDayOfWeek.setTime(previousDayOfWeekTimestamp);
+
+    return previousDayOfWeek;
+  };
+
+  const formatMonthAndDate = (date: Date) => {
+    const month = date.toLocaleString("en-us", { month: "short" });
+    const day = date.getDate();
+    return `${month} ${day}`;
+  };
+
+  const getPreviousWeekDays = (startOfPreviousWeek: Date) => {
+    const weekDays = [];
+
+    for (let i = 0; i < 7; i++) {
+      const day = new Date(startOfPreviousWeek);
+      day.setDate(startOfPreviousWeek.getDate() + i);
+      weekDays.push(formatMonthAndDate(day));
+    }
+
+    return weekDays;
+  };
+
+  const startOfPreviousWeek = getStartOfPreviousWeek();
+  const previousDatesOfWeek = getPreviousWeekDays(startOfPreviousWeek);
+
+  return previousDatesOfWeek;
+};
