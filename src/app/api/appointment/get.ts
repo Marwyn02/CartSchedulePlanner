0f9c2d12a0783f41688414fdd4d82cd5ef57dc29
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-03-04 22:48:00 +0800

    Changes: minor change on  get.ts

--- src/app/api/appointment/get.ts
+++ src/app/api/appointment/get.ts
@@ -5,11 +5,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function getAppointment() {
-  const appointments: any = await prisma.appointment.findMany({
-    include: { user: true },
+  const appointment = await prisma.appointment.findMany({
+    include: {
+      user: true,
+    },
   });
 
   return {
-    schedules: appointments,
+    schedules: appointment,
   };
 }
