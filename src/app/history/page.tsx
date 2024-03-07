022b4580bb234b7321ce2f712cb22e4b098f2838
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-03-07 21:26:39 +0800

    Changes: history page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -2,9 +2,19 @@ import React from "react";
 
 import HistoryTable from "../components/Table/HistoryTable";
 import getAppointment from "../api/appointment/get";
+import { LinkButton } from "../components/ui/button";
 
-const page = async () => {
-  const { schedules } = await getAppointment();
+type TAppointmentProps = {
+  schedules: {
+    id: number;
+    date: string;
+    place: string;
+    time: string | null;
+  }[];
+};
+
+export default async function HistoryPage() {
+  const { schedules }: TAppointmentProps = await getAppointment();
 
   const filteredTimeMorning = schedules.filter(
     (s: any) => s.time === "Morning"
@@ -14,8 +24,11 @@ const page = async () => {
     (s: any) => s.time === "Afternoon"
   );
   return (
-    <div>
-      This is the page
+    <div className="md:p-5 py-5">
+      <div className="flex justify-between items-center px-3">
+        <p className="font-semibold px-2 underline">History logs</p>
+        <LinkButton href="/" text="Back" />
+      </div>
       <HistoryTable
         key={"morning"}
         selectedTime={"Morning"}
@@ -28,6 +41,4 @@ const page = async () => {
       />
     </div>
   );
-};
-
-export default page;
+}
