2e4634bcdaa656bb767f3ac6c5430ad786591f5b
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-03-07 21:27:19 +0800

    Changes: Adjusted the history table

--- src/app/components/Table/HistoryTable.tsx
+++ src/app/components/Table/HistoryTable.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 import { oldDate } from "@/app/api/date";
-import TableBody from "./TableBody";
+import { TableBody } from "./TableBody";
 
 type TAppointmentProps = {
   id: number;
   date: string;
   place: string;
-  time: string;
-  username: string;
+  time: string | null;
 }[];
 
 const HistoryTable = ({
@@ -22,7 +21,9 @@ const HistoryTable = ({
 
   return (
     <div>
-      <p>{selectedTime}</p>
+      <p className="mt-5 px-4 text-sm font-bold pb-2 text-gray-800 uppercase dark:text-gray-200">
+        {selectedTime}
+      </p>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 bg-gray-100">
           <TableBody schedules={appointments} days={weekDates} />
