218ec22c1bfb1347aa1bbe1ff3cfdd7045930f32
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-03-13 18:04:48 +0800

    Changes: Removes unwanted code in schedule page

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import { ScheduleForm } from "../components/Form/ScheduleForm";
 
 const SchedulePage = () => {
-  // async function createAppointment() {
-  //   "use server";
-  // }
   return <ScheduleForm />;
 };
 
